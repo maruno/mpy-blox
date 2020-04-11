076fbc807a3948c486c68933f7d9d9f51c750cb3
Michel Bouwmans <m.bouwmans@ep-games.eu>
2020-04-11 20:10:51 +0200

    NovaPM: Use asyncio StreamReader

--- mpy_blox/sensor/nova_pm.py
+++ mpy_blox/sensor/nova_pm.py
@@ -1,32 +1,40 @@
 import logging
+from uasyncio import StreamReader
 from machine import UART
 
 class NovaPM:
     def __init__(self, uart):
         self.uart = uart
+        self.sr = None
 
     def start(self):
-        self.uart.init(9600, timeout=5000)
+        uart = self.uart
+        uart.init(9600, timeout=5000)
+        self.sr = StreamReader(uart)
 
     def stop(self):
         self.uart.deinit()
 
-    def read(self):
-        msg = self.uart.read(10)
-        logging.debug("NovaPM: Got message %s", msg)
-        assert msg[0] == ord(b'\xaa')
-        assert msg[1] == ord(b'\xc0')
-        assert msg[9] == ord(b'\xab')
-        
-        checksum = sum(v for v in msg[2:8]) % 256
-        assert checksum == msg[8]
+    async def read(self):
+        valid_msg = False
+        while not valid_msg:
+            msg = await self.sr.read(10)
+            if not msg:
+                continue
+            valid_msg = (msg[0] == ord(b'\xaa')
+                         and msg[1] == ord(b'\xc0')
+                         and msg[9] == ord(b'\xab'))
+            if valid_msg:
+                checksum = sum(v for v in msg[2:8]) % 256
+                valid_msg = checksum == msg[8]
 
+        logging.debug("NovaPM: Got message %s", msg)
         pm2_5 = (msg[3] * 256 + msg[2]) / 10.0
         pm10 = (msg[5] * 256 + msg[4]) / 10.0
         return (pm2_5, pm10)
 
 
-def main():
+async def main():
     from machine import UART
     nova_pm = NovaPM(UART(1, rx=4, tx=15))
     nova_pm.start()
@@ -36,4 +44,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    import uasyncio as asyncio
+    asyncio.run(main())
