b2b9959e0e50187de7a0e85bddc56d1a05671c30
Maruno Ulfdrengr <mfb.chikazuku@gmail.com>
2022-06-26 21:03:27 +0200

    Network: Add sleep, fixing WiFi configuration crashing

--- mpy_blox/network.py
+++ mpy_blox/network.py
@@ -10,11 +10,16 @@ from utime import sleep
 def connect_wlan(config):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
+
+    sleep(0.1)  # Seems to be needed to activate interface...
     wlan.config(dhcp_hostname=config.get('hostname', 'espressif'))
 
     try:
         secure_cfg = config['secure']
-        wlan.connect(secure_cfg['wlan.ssid'], secure_cfg['wlan.psk'])
+        ssid = secure_cfg['wlan.ssid']
+
+        logging.info("Connecting to SSID %s", ssid)
+        wlan.connect(ssid, secure_cfg['wlan.psk'])
     except KeyError:
         logging.warning("Network credentials not found. Device provisioned? "
                         "Can't boot with network support")
