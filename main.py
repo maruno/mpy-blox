75c2c2421c88bd4c12b745f9b200585ac2ed50d9
Maruno Ulfdrengr <mfb.chikazuku@gmail.com>
2022-07-02 23:19:23 +0200

    Use a new bare-bones device main that shouldn't change much

---
+++ main.py
@@ -0,0 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from mpy_blox.app import main_except_reset
+
+
+if __name__ == '__main__':
+    main_except_reset()
