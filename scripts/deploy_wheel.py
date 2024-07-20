934c3ff5adcd30b75f120fb777ffd59058947515
Maruno Ulfdrengr <mfb.chikazuku@gmail.com>
2024-07-20 19:06:06 +0200

    Deploy: Use flash storage temporarily to fix robustness

--- scripts/deploy_wheel.py
+++ scripts/deploy_wheel.py
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import gc
 import logging
+import os
 
 import mpy_blox.wheel as wheel
 from mpy_blox.wheel.wheelfile import WheelFile
@@ -10,13 +12,17 @@ from mpy_blox.wheel.wheelfile import WheelFile
 logging.basicConfig(level=logging.INFO)
 
 logging.info("Opening WheelFile")
-with open('/remote/dist/mpy_blox-latest-mpy6-bytecode-esp32.whl', 'rb') as f:
+with open('/dist/mpy_blox-latest-mpy6-bytecode-esp32.whl', 'rb') as f:
     wheel_file = WheelFile(f)
     try:
+        gc.collect()
         wheel.install(wheel_file)
     except wheel.WheelExistingInstallation as ex_install_exc:
         logging.info("Force upgrading existing installation "
                      "{} -> {}".format(
                          ex_install_exc.existing_pkg.metadata['Version'],
                          wheel_file.package.metadata['Version']))
+        gc.collect()
         wheel.upgrade(ex_install_exc.existing_pkg, wheel_file)
+
+os.remove('/dist/mpy_blox-latest-mpy6-bytecode-esp32.whl')
