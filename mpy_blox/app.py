c1db628414ec9f09f69c68c01da74240ff60c36f
Maruno Ulfdrengr <mfb.chikazuku@gmail.com>
2024-10-05 15:46:25 +0200

    App: Log VFS and memory heap info on boot

--- mpy_blox/app.py
+++ mpy_blox/app.py
@@ -17,6 +17,7 @@ from mpy_blox.mqtt.update import MQTTUpdateChannel
 from mpy_blox.network import connect_wlan
 from mpy_blox.syslog import init_syslog
 from mpy_blox.time import sync_ntp
+from mpy_blox.util import log_vfs_state, log_mem_state
 
 
 def start_network(config):
@@ -65,6 +66,8 @@ def main():
     # We are booted, no more need for kernel messages
     osdebug(None)
     logging.info('Mpy-BLOX: Core succesfully booted')
+    log_vfs_state('/')
+    log_mem_state()
 
     if network_available:
         logging.info("MPy-BLOX: Network available, connecting MQTT")
