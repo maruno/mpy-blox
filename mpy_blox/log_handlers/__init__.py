2c27b9f5d0a309b7bdef58164200d52a4d4b5948
Maruno Ulfdrengr <mfb.chikazuku@gmail.com>
2024-10-14 05:19:32 +0200

    LogHandlers: Fix logger for blox_log_config`

--- mpy_blox/log_handlers/__init__.py
+++ mpy_blox/log_handlers/__init__.py
@@ -2,7 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import logging
+from logging import getLogger
+
+
+logger = getLogger('system')
 
 
 async def blox_log_config(settings, network_available):
@@ -11,12 +14,14 @@ async def blox_log_config(settings, network_available):
         if 'logging.syslog.hostname' in settings:
             from mpy_blox.log_handlers.syslog import init_syslog
             try:
+                logger.info("Trying to initialise syslog")
                 init_syslog(settings)
             except Exception:
-                logging.warning("Failed to initialise syslog")
+                logger.warning("Failed to initialise syslog")
         if 'logging.remote_terminal.listen_host' in settings:
             from mpy_blox.log_handlers.remote_terminal import init_remote_terminal
             try:
+                logger.info("Trying to initialise remote terminal")
                 await init_remote_terminal(settings)
             except Exception:
-                logging.warning("Failed to initialise remote terminal")
+                logger.warning("Failed to initialise remote terminal")
