11b73af5deb1c46161868506697b9cebbf4d0a35
Maruno Ulfdrengr <mfb.chikazuku@gmail.com>
2025-03-16 14:56:52 +0100

    MQTT.protocol: Fix missing MPL-2.0 licensing headers

--- mpy_blox/mqtt/protocol/exc.py
+++ mpy_blox/mqtt/protocol/exc.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 class ErrorWithMQTTReason(Exception):
     def __init__(self, reason_code, *args: object) -> None:
         super().__init__(*args)
